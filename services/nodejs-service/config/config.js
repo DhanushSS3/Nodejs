adc273e6a11b24b9d425900695fdd5ededba8e02
DhanushSS3 <support@livefxhub.com>
2025-09-23 04:01:54 -0700

    Corrected worker close error

--- services/nodejs-service/config/config.js
+++ services/nodejs-service/config/config.js
@@ -1,11 +1,12 @@
-console.log("Loading hardcoded values");
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: "u436589492_forex",
-    password: "Setupdev@1998",
-    database: "u436589492_forex",
-    host: "127.0.0.1",
-    port: 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mariadb",
   },
 };
